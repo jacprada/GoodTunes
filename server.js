15407398dd0747e3878563f01f8d37e388e06e85
jacprada <jacopo.prada@gmail.com>
2016-02-27 15:41:08 +0000

    passport and passport local modules added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Saving dependencies into variables to make them accessible
 var express           = require('express');
 var app               = express();
 var cors              = require('cors');
@@ -5,8 +6,18 @@ var bodyParser        = require('body-parser');
 var morgan            = require('morgan');
 var mongoose          = require('mongoose');
 
+// Setting view folder and serving files from the public folder
 app.set("views", "./public");
 app.use(express.static(__dirname + '/public'));
 
+// Setting body parsing middleware for json and URL-encoded data 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Setting local and online database
+var databaseURL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/startup-ecosystems'
+mongoose.connect(databaseURL);
+
+// Setting cors middleware and HTTP request logger middleware
+app.use(cors());
+app.use(morgan('dev'));
